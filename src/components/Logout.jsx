8ffbb7c6713976ef41f86dc088cd2ccc74cbdf21
lkoczyba <lukasz.koczyba@gmail.com>
2023-11-02 19:16:06 +0100

    logout section finished

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Heading from "./Heading.jsx";
+import {NavLink} from "react-router-dom";
 
 const Logout = () => {
     return (
-        <div>
-            Logout placeholder
+        <div className='logout'>
+            <Heading text1='wylogowanie nastąpiło' text2='pomyślnie'/>
+            <NavLink className="button button--small button--border" to={"/"}>Strona główna</NavLink>
         </div>
     );
 };
